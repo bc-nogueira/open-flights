9e01deec1edd547a0cf76d1e3c66089cf9d105ca
bnogueira <bnogueira@gannett.com>
2021-02-12 23:37:03 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# OpenFlights 
+### A flight reviews app built with Ruby on Rails and React.js
+
+This app was made following the [video tutorial](https://www.youtube.com/watch?v=oyjzi837wME&t) from [Zayne's Youtube channel](https://www.youtube.com/channel/UCkoEStUK7wxmZef2DcPuCAQ).
+
+---
+
+## This app uses
+
+* Ruby version: `2.7.0`
+* Rails version: `6.0.3.4`
+* React version: `17.0.1`
+
+## Running it locally
+- run `bundle exec rails db:prepare`
+- run `npm install` or `yarn install`
+- run `bundle exec rails s`
+- in another tab run `./bin/webpack-dev-server` (optional) 
+- access the app at `http://localhost:3000`
---
+++ app/javascript/components/Airline/Airline.js
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+
+import Header from './Header'
+import Review from './Review'
+import ReviewForm from './ReviewForm'
+
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`
+
+const Column = styled.div`
+  background: #fff;
+  height: 100vh;
+  overflow: scroll;
+
+  &:last-child {
+    background: #000;
+  }
+`
+
+const Main = styled.div`
+  padding-left: 50px;
+`
+
+const Airline = (props) => {
+  const [airline, setAirline] = useState({})
+  const [review, setReview] = useState({})
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    const slug = props.match.params.slug
+    const url = `/api/v1/airlines/${slug}`
+    
+    axios.get(url)
+      .then(resp => {
+        setAirline(resp.data)
+        setLoaded(true)
+        console.log(resp.data)
+      })
+      .catch(resp => console.log(resp))
+  }, [])
+
+  const handleChange = (e) => {
+    e.preventDefault()
+
+    // setReview(Object.assign({}, review, { [e.target.name]: e.target.value }))
+    setReview({ ...review, [e.target.name]: e.target.value })
+
+    // console.log('review: ', review)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const csrfToken = document.querySelector('[name=csrf-token]').content
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+
+    const airline_id = airline.data.id
+    axios.post('/api/v1/reviews', { review, airline_id })
+      .then(resp => {
+        const included = [...airline.included, resp.data.data]
+        setAirline({ ...airline, included })
+        setReview({ title: '', description: '', score: 0 })
+      })
+      .catch(resp => {})
+  }
+
+  const setRating = (score, e) => {
+    e.preventDefault()
+
+    setReview({ ...review, score })
+  }
+
+  let reviews
+  if(loaded && airline.included) {
+    reviews = airline.included.map((item, index) => {
+      return (
+        <Review
+          key={index}
+          attributes={item.attributes}
+        />
+      )
+    })
+  }
+
+  return (
+    <Wrapper>
+      {loaded &&
+      <>
+        <Column>
+          <Main>
+            <Header
+              attributes={airline.data.attributes}
+              reviews={airline.included}
+            />
+          {reviews}
+          </Main>
+        </Column>
+        <Column>
+          <ReviewForm
+            handleChange={handleChange}
+            setRating={setRating}
+            handleSubmit={handleSubmit}
+            attributes={airline.data.attributes}
+            review={review}
+          />
+        </Column>
+      </>
+      } 
+    </Wrapper>
+  )
+}
+
+export default Airline
